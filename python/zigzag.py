3ce72bd88e4e5d54ad0918d2fdb0cd298c3b7f94
freddy velarde <freddyvelarde59@gmail.com>
2022-12-11 18:26:53 -0400

    zigzag traverse

--- python/zigzag.py
+++ python/zigzag.py
@@ -1,7 +1,52 @@
 
-def zigzag(n, m):
-    matrix = [ [0 for _ in range(n)] for _ in range(m) ]
+
+def printArr(arr):
+    res = ''
+    for num in arr:
+        res += str(num) + ' '
+    return print(res[:-1])
+
+
+def zigZagTraverse(n, m):
+    matrix = [[0 for _ in range(m)] for _ in range(n)]
     
+    row, col = 0, 0
+
+    counter = 1
+    going_up = False
+
+    while row < n and row >= 0 and col < m and col >= 0:
+        if going_up:
+            i = row 
+            while i >= 0 and col < m:
+                matrix[i][col] = counter
+                counter += 1
+                col += 1
+                i -= 1
+            if col == m:
+                col -= 1
+                row = i + 2
+            else:
+                row = i + 1
+        else: 
+            i = row 
+            while i < n and col >= 0:
+                matrix[i][col] = counter
+                counter += 1
+                col -= 1
+                i += 1
+            if i == n:
+                row = i - 1
+                col += 2
+            else:
+                row = i 
+                col += 1
+        going_up = not going_up
+    
+    for arr in matrix:
+        printArr(arr)
 
 
-zigzag(4, 5)
+# RUNNIG CODE
+n,m  = map(int, input().split())
+zigZagTraverse(n, m)

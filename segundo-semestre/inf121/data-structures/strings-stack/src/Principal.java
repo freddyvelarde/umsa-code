1792d3e6e81252ead7fc7ac072283aeeae3d9ff4
freddyvelarde <freddyvelarde59@gmail.com>
2023-08-30 21:24:22 -0400

    Adding my code

--- segundo-semestre/inf121/data-structures/strings-stack/src/Principal.java
+++ segundo-semestre/inf121/data-structures/strings-stack/src/Principal.java
@@ -5,8 +5,33 @@ public class Principal {
 
     names.llenar(3);
 
-    names.mostrar();
+    Pila<String> longestStrings = findLongestStrings(names);
+    longestStrings.mostrar();
+
     // System.out.println(vowelsCounter(names));
+  }
+
+  static Pila<String> findLongestStrings(Pila<String> names) {
+    Pila<String> swapStack = new Pila<>();
+    Pila<String> longestStrings = new Pila<>();
+    int maxLength = 0;
+
+    while (!names.esVacia()) {
+      String name = names.eli();
+      int length = name.length();
+
+      if (length > maxLength) {
+        maxLength = length;
+        longestStrings = new Pila<>();
+        longestStrings.adi(name);
+      } else if (length == maxLength) {
+        longestStrings.adi(name);
+      }
+
+      swapStack.adi(name);
+    }
+    names.vaciar(swapStack);
+    return longestStrings;
   }
 
   static int vowelsCounter(Pila<String> names) {
